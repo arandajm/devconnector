4ab1d65a15fb8902b7be6540c3bc70cb70dc2142
Jesus Aranda <aranda.jm@icloud.com>
2018-12-14 22:03:17 +0000

    create user register route. Integrate with gravatar and bcriptjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 //@route GET /api/users/test
 //@desc Test user route
@@ -7,5 +10,50 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: 'Users works'
 }));
+
+
+//@route POST /api/users/register
+//@desc Register user route
+//@access Public
+router.post('/register', (req, res) => {
+    console.log('Entro!!!');
+    // Check if user with email exists
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if (user) {
+            return res.status(400).json({
+                email: 'Email already exists!!'
+            })
+        } else {
+            // Get avatar with Gravatar
+            const avatar = gravatar.url(req.body.email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            // Create new Resource
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+
+            // Hash password with bcryptjs
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err
+                    newUser.password = hash;
+                    // Save New User
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    }).catch(err => console.log(err));
+});
 
 module.exports = router;
