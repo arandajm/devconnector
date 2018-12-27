5f3a1afb366b6f27536f8fb9e47ea33699aa8b9f
Jesus Aranda <aranda.jm@icloud.com>
2018-12-27 20:36:30 +0000

    create others profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,6 +18,71 @@ router.get("/test", (req, res) =>
     })
 );
 
+//@route GET /api/profile/all
+//@desc Get all profile user
+//@access Public
+router.get('/all', (req, res) => {
+    const errors = {};
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if (!profiles) {
+                errors.noprofiles = 'There are no profiles for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profiles);
+        })
+        .catch(err => {
+            errors.noprofiles = 'There are no profiles for this user';
+            res.status(404).json(errors);
+        })
+});
+
+//@route GET /api/profile/handle/:handle
+//@desc Get profile user by handle
+//@access Public
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+    Profile.findOne({
+            handle: req.params.handle
+        })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => {
+            errors.noprofile = 'There is no profile for this user';
+            res.status(404).json(errors);
+        })
+});
+
+//@route GET /api/profile/user/:user_id
+//@desc Get profile user by user_id
+//@access Public
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+    Profile.findOne({
+            user: req.params.user_id
+        })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors)
+            }
+
+            res.json(profile);
+        })
+        .catch(err => {
+            errors.noprofile = 'There is no profile for this user';
+            res.status(404).json(errors);
+        })
+});
+
 //@route GET /api/profile
 //@desc Get current user profile
 //@access Private
