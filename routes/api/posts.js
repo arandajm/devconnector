da1bd6dad03224f9a79728dcfab795208ede78f8
Jesus Aranda <aranda.jm@icloud.com>
2018-12-31 11:58:37 +0000

    create post route and it's validations

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require("../../models/Post");
+
+// Import post validation
+const validatePostInput = require('../../validation/post');
 
 //@route GET /api/posts/test
 //@desc Test post route
@@ -7,5 +14,39 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: 'Posts works'
 }));
+
+//@route POST /api/posts
+//@desc Create a new post
+//@access Private
+router.post('/', passport.authenticate("jwt", {
+    session: false
+}), (req, res) => {
+
+    const {
+        errors,
+        isValid
+    } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    });
+
+    // Save the new post
+    newPost.save()
+        .then(post => res.json(post))
+        .catch(err => res.status(400).json({
+            err: err
+        }));
+
+
+})
 
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,25 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+    let errors = {};
+
+    data.text = !isEmpty(data.text) ? data.text : '';
+
+    if (!Validator.isLength(data.text, {
+            min: 10,
+            max: 30
+        })) {
+        errors.text = 'Post text field must be between 10 to 30 characters!';
+    }
+
+    if (Validator.isEmpty(data.text)) {
+        errors.text = 'Text field is required';
+    }
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
