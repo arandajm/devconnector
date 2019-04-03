ed36043a69757bcfd11c2a99d1e2046043ace4de
Jesus Aranda <aranda.jm@icloud.com>
2019-04-03 14:34:28 +0100

    bug fixing in register form validation conditions

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
+import classnames from "classnames";
 
 class Register extends Component {
   constructor() {
     super();
-    //Create register state
+    // Create register state
     this.state = {
       name: "",
       email: "",
@@ -35,10 +36,16 @@ class Register extends Component {
     axios
       .post("/api/users/register", newUser)
       .then(response => console.log(response.data))
-      .catch(err => console.log(err.response.data));
+      .catch(err => {
+        console.log(err.response.data);
+        this.setState({ errors: err.response.data });
+      });
   }
 
   render() {
+    // Is the same that const errors = this.state.errors
+    const { errors } = this.state;
+
     return (
       <div className="register">
         <div className="container">
@@ -48,11 +55,13 @@ class Register extends Component {
               <p className="lead text-center">
                 Create your DevConnector account
               </p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.name
+                    })}
                     placeholder="Name"
                     name="name"
                     // Link state with the field
@@ -60,11 +69,17 @@ class Register extends Component {
                     // Bind field with onChange event
                     onChange={this.onChange}
                   />
+                  {errors.name && (
+                    // If errors.name has a value, show the error message
+                    <div className="invalid-feedback">{errors.name}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     // Link state with the field
@@ -72,6 +87,10 @@ class Register extends Component {
                     // Bind field with onChange event
                     onChange={this.onChange}
                   />
+                  {errors.email && (
+                    // If errors.email has a value, show the error message
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
@@ -80,7 +99,9 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     // Link state with the field
@@ -88,11 +109,17 @@ class Register extends Component {
                     // Bind field with onChange event
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    // If errors.password has a value, show the error message
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password2
+                    })}
                     placeholder="Confirm Password"
                     name="password2"
                     // Link state with the field
@@ -100,6 +127,10 @@ class Register extends Component {
                     // Bind field with onChange event
                     onChange={this.onChange}
                   />
+                  {errors.password2 && (
+                    // If errors.password has a value, show the error message
+                    <div className="invalid-feedback">{errors.password2}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
