d5084edbab517611025a6ddacc020fab4d5cbc6b
Jesus Aranda <aranda.jm@icloud.com>
2019-04-22 18:57:10 +0100

    Create Profile actions component and delete button in dashboard

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,7 +3,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from "./types";
 
 // Get Current Profile
@@ -43,6 +44,27 @@ export const createProfile = (profileData, history) => dispatch => {
         payload: err.response.data
       });
     });
+};
+
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!!")) {
+    // Ajax call
+    axios
+      .delete("/api/profile")
+      .then(response => {
+        console.log("Delete account successfully...");
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  }
 };
 
 // Set profile user
