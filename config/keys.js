f2c052296683d2c20cdbe74c78c5d5d73609e53a
Jesus Aranda <aranda.jm@icloud.com>
2019-05-16 16:41:42 +0100

    Securing our keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-// Make visiable this object outside this file
-module.exports = {
-    mongoURI: 'mongodb://jesus:123456a@ds011422.mlab.com:11422/devconnector',
-    secretOrKey: 'secret'
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
 }
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+// Make visiable this object outside this file
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
