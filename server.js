14a7650f924a6c2735d21dc5b08cc2e8a54cc85b
Jesus Aranda <aranda.jm@icloud.com>
2019-05-27 18:43:30 +0100

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,41 +1,54 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = new express();
 
-// Body parser Middleware    
-app.use(bodyParser.urlencoded({
+// Body parser Middleware
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(bodyParser.json());
 
 // Passport Middleware
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-// DB Config 
-const db = require('./config/keys').mongoURI;
+// DB Config
+const db = require("./config/keys").mongoURI;
 
-// Monggose connection return a js promise 
-mongoose.connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log(err));
+// Monggose connection return a js promise
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello!!!!!!!')
+app.get("/", (req, res) => {
+  res.send("Hello!!!!!!!");
 });
 
 //Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+//Serve static assests if is in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  //For any frontend route, serve index file
+  app.get("*", (req, res) => {
+    //Send index file
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Heroku port set process.env.PORT, default 5000;
 const port = process.env.PORT || 5000;
