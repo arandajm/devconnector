0ce5da96d9f18d072c0f536ad9e033c5fdd3cfe7
Jesus Aranda <aranda.jm@icloud.com>
2020-04-02 18:37:30 -0300

    updating layout component using hooks (functional components)

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; 2018 Dev Connector
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Dev Connector
+    </footer>
+  );
+};
+
 export default Footer;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,50 +1,43 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
-class Landing extends Component {
-  componentDidMount() {
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/dashboard");
+const Landing = ({ history }) => {
+  const auth = useSelector(state => state.auth);
+
+  // use instead of life circle methods like component componentDidMount
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      // if the user is authenticated, go to dashboard
+      history.push("/dashboard");
     }
-  }
+  });
 
-  render() {
-    return (
-      <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
-                <p className="lead">
-                  {" "}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
-                </p>
-                <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </Link>
-                <Link to="/login" className="btn btn-lg btn-light">
-                  Login
-                </Link>
-              </div>
+  return (
+    <div className="landing">
+      <div className="dark-overlay landing-inner text-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <h1 className="display-3 mb-4">Developer Connector</h1>
+              <p className="lead">
+                {" "}
+                Create a developer profile/portfolio, share posts and get help
+                from other developers
+              </p>
+              <hr />
+              <Link to="/register" className="btn btn-lg btn-info mr-2">
+                Sign Up
+              </Link>
+              <Link to="/login" className="btn btn-lg btn-light">
+                Login
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
-
-Landing.propTypes = {
-  auth: PropTypes.object.isRequired
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth
-});
-
-export default connect(mapStateToProps)(Landing);
+export default Landing;
