b2083c1eace1284ad948e1cdfd272891b977a588
Jesus Aranda <aranda.jm@icloud.com>
2020-04-05 17:12:38 -0300

    Renaming the set Auht Token util file and adding code comments

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import setAuthToken from "../utils/serAuthToken";
+import setAuthToken from "../utils/setAuthToken";
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import jwt_decode from "jwt-decode";
 
--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const setAuthToken = token => {
   if (token) {
-    // Apply to every request
+    // Add authorization token like a default header to any request
     axios.defaults.headers.common["Authorization"] = token;
   } else {
+    // if it exists, delete it!
     delete axios.defaults.headers.common["Authorization"];
   }
 };
